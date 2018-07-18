d7700a280ed4c036952783b092711f68b22b15ff
Ed Shell <shelleg@g.cofc.edu>
2018-07-18 14:10:14 -0400

    files

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "lpn6ooq5xkf42mx0",
-    password: "uzhynsxtic8dq028",
+    user: "root",
+    password: "root",
     database: "burgers_db"
   });
 }
