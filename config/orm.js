9d6a43a386040fb57129465ad1418acf81f76b96
Ed Shell <shelleg@g.cofc.edu>
2018-07-17 21:17:49 -0400

    files

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,36 @@
 var connection = require("../config/connection.js");
 
-// function printQuestionMarks(num) {
-//   var arr = [];
+function printQuestionMarks(num) {
+  var arr = [];
 
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-//   return arr.toString();
-// }
+  return arr.toString();
+}
 
-// function objToSql(ob) {
-//   var arr = [];
+function objToSql(ob) {
+  var arr = [];
 
-//   loop through the keys and push the key/value as a string int arr
-//   for (var key in ob) {
-//     var value = ob[key];
-//     check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(key + "=" + value);
-//     }
-//   }
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
 
-//   translate array of strings to a single comma-separated string
-//   return arr.toString();
-//
-
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 // var orm = {}
 
 // Inisde curly brace
@@ -40,7 +39,7 @@ var connection = require("../config/connection.js");
 var orm = {
   selectAll: function(table, cb) {
     var queryString = "SELECT * FROM " + table + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function(err, results) {
       if (err) {
         throw err;
       }
@@ -65,6 +64,23 @@ var orm = {
         throw err;
       }
 
+      cb(results);
+    });
+  },
+
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
       cb(result);
     });
   }
